b676d0917a472be399b055d430b5903550c6833f
silabanjames <silaban.james13@gmail.com>
2025-05-22 10:09:10 +0800

    chore: add second view to show animation

---
+++ ARExplore/CheckAnimation.swift
@@ -0,0 +1,18 @@
+//
+//  CheckAnimation.swift
+//  ARExplore
+//
+//  Created by James Silaban on 22/05/25.
+//
+
+import SwiftUI
+
+struct CheckAnimation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckAnimation()
+}
